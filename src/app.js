29d65e0f0222e1b11e6d36d1fbc725c31766655d
KateRiinaO <orrava.kate@gmail.com>
2022-03-02 23:20:57 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Riga&appid=faf90a5bc03e42145a435bf92d2bbff7&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
