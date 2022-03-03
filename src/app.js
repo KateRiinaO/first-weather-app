954bc04a99970d6bd509a4a0ebae99ff83fc2d70
KateRiinaO <orrava.kate@gmail.com>
2022-03-03 13:47:50 +0200

    Added relevant weather icon (api)

--- src/app.js
+++ src/app.js
@@ -39,11 +39,13 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "Bali";
+
 let apiKey = "468f1b399930325da4fbd01df40e0c20";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${Moscow}&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
